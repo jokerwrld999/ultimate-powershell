3e6536d0091f12e58c0d50724ad64babd528ac26
jokerwrld999 <ifalaleev49@gmail.com>
2023-03-15 22:15:29 +0200

    Small Fixes

--- setup.ps1
+++ setup.ps1
@@ -28,7 +28,7 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
 }
 # If the file already exists, show the message and do nothing.
 else {
-    Get-Item -Path $PROFILE | Remove-Item $PROFILE
+    Remove-Item -Path $PROFILE
     Invoke-RestMethod https://github.com/jokerwrld999/ultimate-powershell/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
     Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
 }
