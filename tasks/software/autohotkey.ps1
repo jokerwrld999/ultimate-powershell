8c864637c64e6cf8d0266c00c248ebedb5ea02c1
jokerwrld999 <ifalaleev49@gmail.com>
2024-01-22 20:55:36 +0200

    .ahk file association

--- tasks/software/autohotkey.ps1
+++ tasks/software/autohotkey.ps1
@@ -19,12 +19,24 @@ $Shortcut.Save()
 
 Write-Host "The AutoHotkey script @ [$autoHotkeyScriptPath] has been created, and a shortcut was created in the Startup folder." -f Green
 
-Write-Host "Pathing AutoHotkey..." -f Blue
+Write-Host "Patching AutoHotkey..." -f Blue
 $pattern = '^\s*(C:\\.*\\autohotkey\\\d.*$)'
 $autoHotkeyInstallPath = (powershell -c "scoop info autohotkey") -match $pattern
 $autoHotkeySourcePath = [System.IO.Path]::Combine($autoHotkeyInstallPath.Trim(), "UX\inc\identify.ahk")
 Invoke-WebRequest -Uri "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/autohotkey/identify_fix.ahk" -OutFile $autoHotkeySourcePath
 Write-Host "AutoHotkey was successfully patched @ [$autoHotkeySourcePath]." -f Green
+
+if (!(Get-ItemProperty -Path "HKCU\Software\Classes\.ahk" -Name (Default) -ErrorAction SilentlyContinue).Value -match "AutoHotkey.ahk") {
+    Write-Host "Setting .ahk association to Registry..." -f Blue
+    Set-ItemProperty -Path "HKCU\Software\Classes\.ahk" -Name (Default) -Value "AutoHotkey.ahk"
+    New-ItemProperty -Path "HKCU\Software\Classes\.ahk\shell\open\command" -Name (Default) -Value "$env:userprofile\scoop\apps\autohotkey\current\v2\AutoHotkey64.exe"
+
+    New-ItemProperty -Path "HKCU\Software\Classes\.ahk\shell\open\command" -Name (Default) -Value "runas /user:administrator $env:userprofile\scoop\apps\autohotkey\current\v2\AutoHotkey64.exe"
+
+    Write-Host ".ahk association is successfully set" -f Green
+} else {
+  Write-Host ".ahk association already set in Registry." -f Green
+}
 
 Write-Host "Starting AutoHotkey script..." -f Blue
 Start-Process -FilePath "$env:USERPROFILE\scoop\shims\autohotkey.exe" -ArgumentList "$autoHotkeyScriptPath" -NoNewWindow *>$null
--- tasks/software/sharex.ps1
+++ tasks/software/sharex.ps1
@@ -11,7 +11,7 @@ $Shortcut.TargetPath = $sharexAppPath
 $Shortcut.Save()
 
 Write-Host "Starting ShareX..." -f Blue
-Start-Process -FilePath $sharexAppPath -WindowStyle Minimized *>$null
+Start-Process -FilePath $sharexAppPath -WindowStyle Hidden *>$null
 Write-Host "The ShareX app has been started and shortcut was created at the Startup folder." -f Green
 
 Write-Host "Restoring ShareX backup..." -f Blue
