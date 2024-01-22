2cec29914b57c7581fe66606c54e3bf439baea54
jokerwrld <ifalaleev49@gmail.com>
2024-01-22 17:48:47 +0200

    Scoop updates idempotency fix

--- tasks/main.ps1
+++ tasks/main.ps1
@@ -1,22 +1,22 @@
 
 # >>> Running Tweaks
-Write-Host "####### Running Tweaks....... #######" -f Blue
+Write-Host "####### Running Tweaks....... #######" -f Cyan
 irm "https://raw.githubusercontent.com/jokerwrld999/ultimate-powershell/main/tasks/system_setup/tweaks.ps1" | iex
 
 # >>> Installing Scoop Packages
-Write-Host "####### Installing Scoop Packages....... #######" -f Blue
+Write-Host "####### Installing Scoop Packages....... #######" -f Cyan
 irm "https://raw.githubusercontent.com/jokerwrld999/ultimate-powershell/main/tasks/software/scoop_packages.ps1" | iex
 
 # >>> Setting Up Oh-My-Posh
-Write-Host "####### Setting Up Oh-My-Posh....... #######" -f Blue
+Write-Host "####### Setting Up Oh-My-Posh....... #######" -f Cyan
 irm "https://raw.githubusercontent.com/jokerwrld999/ultimate-powershell/main/tasks/software/oh_my_posh.ps1" | iex
 
 # >>> Setting Up AutoHotkey
-Write-Host "####### Setting Up AutoHotkey....... #######" -f Blue
+Write-Host "####### Setting Up AutoHotkey....... #######" -f Cyan
 irm "https://raw.githubusercontent.com/jokerwrld999/ultimate-powershell/main/tasks/software/autohotkey.ps1" | iex
 
 # >>> Setting Up ShareX
-Write-Host "####### Setting Up ShareX....... #######" -f Blue
+Write-Host "####### Setting Up ShareX....... #######" -f Cyan
 irm "https://raw.githubusercontent.com/jokerwrld999/ultimate-powershell/main/tasks/software/sharex.ps1" | iex
 
 # >>> Setting Up WSL2
--- tasks/software/scoop_packages.ps1
+++ tasks/software/scoop_packages.ps1
@@ -1,48 +1,87 @@
 # >>> Installing Scoop
 if (![bool](Get-Command -Name 'scoop' -ErrorAction SilentlyContinue)) {
-    Write-Host "Scoop is already installed, skip installation." -f Green
-}
-else {
     Write-Host "Installing Scoop Module..." -f Blue
     iex "& {$(irm get.scoop.sh *>$null)} -RunAsAdmin"
+} else {
+    Write-Host "Scoop is already installed, skip installation." -f Green
 }
 
+# Set Scoop variables
+$scoopAppsBucket = 'Scoop-Apps'
+$scoopMainBucket = 'main'
+$scoopExtrasBucket = 'extras'
+$scoopNonPortableBucket = 'nonportable'
+$scoopVersionsBucket = 'versions'
+$scoopGamesBucket = 'games'
+
+# Add buckets if not already added
+$buckets = @($scoopAppsBucket, $scoopMainBucket, $scoopExtrasBucket, $scoopNonPortableBucket, $scoopVersionsBucket, $scoopGamesBucket)
+foreach ($bucket in $buckets) {
+    if (!(scoop bucket list | Out-String) -contains $bucket) {
+        Write-Host "Adding Scoop bucket: $bucket" -f Blue
+        scoop bucket add $bucket *>$null
+    } else {
+        Write-Host "Scoop bucket $bucket is already added, skip adding." -f Green
+    }
+}
+
+# Set Scoop config
+$scoopRepoUrl = 'https://github.com/Ash258/Scoop-Core'
+if (!(scoop config SCOOP_REPO | Out-String) -eq $scoopRepoUrl) {
+    Write-Host "Setting Scoop repository to $scoopRepoUrl" -f Blue
+    scoop config SCOOP_REPO $scoopRepoUrl *>$null
+} else {
+    Write-Host "Scoop repository is already set to $scoopRepoUrl, skip setting." -f Green
+}
+
+# Update Scoop
 Write-Host "Updating Scoop Module..." -f Blue
-scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache *>$null
-scoop config SCOOP_REPO 'https://github.com/Ash258/Scoop-Core' *>$null
-scoop bucket add Scoop-Apps 'https://github.com/ACooper81/scoop-apps' *>$null
-scoop bucket add main *>$null
-scoop bucket add extras *>$null
-scoop bucket add nonportable *>$null
-scoop bucket add versions *>$null
-scoop bucket add games *>$null
 scoop update *>$null
 
+# Applications to install
 $applications = @(
-    @{ Name = "Adobe Reader"; Id = "AdobeAcrobatReader-Install" },
-    @{ Name = "AutoHotkey"; Id = "extras/autohotkey" },
-    @{ Name = "CoreTemp"; Id = "extras/coretemp" },
+    @{ Name = "AdobeAcrobatReader"; Id = "AdobeAcrobatReader-Install" },
+    @{ Name = "Autohotkey"; Id = "extras/autohotkey" },
+    @{ Name = "Coretemp"; Id = "extras/coretemp" },
     @{ Name = "Discord"; Id = "extras/discord" },
     @{ Name = "Firefox"; Id = "extras/firefox" },
     @{ Name = "Github CLI"; Id = "main/gh" },
     @{ Name = "Google Chrome"; Id = "googlechrome" },
-    @{ Name = "Oh My Posh"; Id = "main/oh-my-posh" },
+    @{ Name = "Oh-My-Posh"; Id = "main/oh-my-posh" },
     @{ Name = "Parsec"; Id = "nonportable/parsec-np" },
-    @{ Name = "Python 3.11"; Id = "versions/python311" },
-    @{ Name = "ShareX"; Id = "extras/sharex" },
-    @{ Name = "Speedtest CLI"; Id = "extras/speedtest" },
+    @{ Name = "Python311"; Id = "versions/python311" },
+    @{ Name = "Sharex"; Id = "extras/sharex" },
+    @{ Name = "Speedtest"; Id = "extras/speedtest" },
     @{ Name = "Steam"; Id = "games/steam" },
     @{ Name = "Tailscale"; Id = "extras/tailscale" },
-    @{ Name = "TelegramDesktop"; Id = "extras/telegram" },
-    @{ Name = "VS Code"; Id = "extras/vscode" },
+    @{ Name = "Telegram"; Id = "extras/telegram" },
+    @{ Name = "VsCode"; Id = "extras/vscode" },
     @{ Name = "qBittorrent"; Id = "extras/qbittorrent" }
 )
 
 foreach ($app in $applications) {
-    Write-Host ("Installing $($app.Name)...") -f Blue
-    scoop install $($app.Id) *>$null
+    if (!(Get-Command -ErrorAction SilentlyContinue -Name $app.Name)) {
+        Write-Host ("Installing $($app.Name)...") -f Blue
+        scoop install $($app.Id) *>$null
+    } else {
+        Write-Host ("$($app.Name) is already installed, skip installation.") -f Green
+    }
 }
 
+# Install Nerd Fonts
 Write-Host("Installing Nerd Fonts...") -f Blue
-scoop bucket add nerd-fonts *>$null
-scoop install Meslo-NF Meslo-NF-Mono Hack-NF Hack-NF-Mono FiraCode-NF FiraCode-NF-Mono FiraMono-NF FiraMono-NF-Mono *>$null
+if (!(scoop bucket list | Out-String) -contains 'nerd-fonts') {
+    Write-Host "Adding Scoop bucket: nerd-fonts" -f Blue
+    scoop bucket add nerd-fonts *>$null
+} else {
+    Write-Host "Scoop bucket nerd-fonts is already added, skip adding." -f Green
+}
+
+$nerdFonts = @('Meslo-NF', 'Meslo-NF-Mono', 'Hack-NF', 'Hack-NF-Mono', 'FiraCode-NF', 'FiraCode-NF-Mono', 'FiraMono-NF', 'FiraMono-NF-Mono')
+foreach ($font in $nerdFonts) {
+    if (!(Get-Command -ErrorAction SilentlyContinue -Name $font)) {
+        scoop install $font *>$null
+    } else {
+        Write-Host ("$font is already installed, skip installation.") -f Green
+    }
+}
