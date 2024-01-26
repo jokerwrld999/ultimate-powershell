1dde0561bcf838f43169b674bb52c0295659773e
jokerwrld <ifalaleev49@gmail.com>
2024-01-26 15:28:18 +0200

    [scoop] Add bucket

--- pwsh.ps1
+++ pwsh.ps1
@@ -1,40 +1,11 @@
-function ScheduleTaskForNextBoot() {
-    Write-Host "Scheduling task for next boot..." -ForegroundColor Blue
 
-    $ActionScript = '& {Invoke-Command -ScriptBlock ([scriptblock]::Create([System.Text.Encoding]::UTF8.GetString((New-Object Net.WebClient).DownloadData(''https://raw.githubusercontent.com/jokerwrld999/ultimate-powershell/main/tasks/system_setup/wsl.ps1'')))) -ArgumentList $true}'
-
-    $Action = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoExit -Command `"$ActionScript`""
-
-    $Trigger = New-ScheduledTaskTrigger -AtLogon
-
-    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-    $Principal = New-ScheduledTaskPrincipal -UserId $currentUser -RunLevel Highest
-
-    Register-ScheduledTask -TaskName "WSL" -Action $Action -Trigger $Trigger -Principal $Principal -Description "Continue Setting Up WSL After Boot" -Force
-}
-
-
-function Get-Confirmation ($message) {
-    $choice = Read-Host "$message (y/N)"
-    if ($choice -eq "y") {
-        return $true
-    } else {
-        return $false
+$buckets = @('main', 'extras', 'nerd-fonts', 'nonportable', 'games', 'Scoop-Apps')
+foreach ($bucket in $buckets) {
+    if (!(scoop bucket list) | Select-string $bucket) {
+        Write-Host "Adding Scoop bucket: $bucket" -f Blue
+        # scoop bucket add $bucket *>$null
+    }
+    else {
+        Write-Host "Skeept buckt... $bucket"
     }
 }
-$restartRequired = $true # $pendingRenameOperations -ne $null
-
-Write-Host "Restart is required: $restartRequired" -ForegroundColor Blue
-if ($restartRequired) {
-    if (Get-Confirmation "Would you like to perform a immediate reboot?") {
-        Write-Host "Rescheduling task for next boot..." -ForegroundColor Blue
-        ScheduleTaskForNextBoot
-        Restart-Computer -force
-    } else {
-        Write-Host "Installation paused. Please reboot manually to complete setup." -ForegroundColor Magenta
-    }
-} else {
-    Write-Host "Features enabled successfully." -ForegroundColor Green
-}
-
-
--- tasks/software/scoop_packages.ps1
+++ tasks/software/scoop_packages.ps1
@@ -7,7 +7,7 @@ $buckets = @('main', 'extras', 'nerd-fonts', 'nonportable', 'games', 'Scoop-Apps
 foreach ($bucket in $buckets) {
     if (!(scoop bucket list) | Select-string $bucket) {
         Write-Host "Adding Scoop bucket: $bucket" -f Blue
-        scoop bucket add $bucket *>$null
+        scoop bucket add $bucket
     }
 }
 
