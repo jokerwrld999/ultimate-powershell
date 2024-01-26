2efe5770b197adb0fcade307176357b71d7e1015
jokerwrld <ifalaleev49@gmail.com>
2024-01-26 15:24:28 +0200

    [oh-my-posh] Modules NuGet

--- tasks/software/scoop_packages.ps1
+++ tasks/software/scoop_packages.ps1
@@ -5,7 +5,7 @@ if (![bool](Get-Command -Name 'scoop' -ErrorAction SilentlyContinue)) {
 
 $buckets = @('main', 'extras', 'nerd-fonts', 'nonportable', 'games', 'Scoop-Apps')
 foreach ($bucket in $buckets) {
-    if (!(scoop bucket list | Out-String) -contains $bucket) {
+    if (!(scoop bucket list) | Select-string $bucket) {
         Write-Host "Adding Scoop bucket: $bucket" -f Blue
         scoop bucket add $bucket *>$null
     }
