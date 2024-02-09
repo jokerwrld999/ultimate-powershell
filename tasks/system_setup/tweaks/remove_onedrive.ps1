ad1f4745da38c037588756eb38195c5d98aaf4a5
jokerwrld999 <ifalaleev49@gmail.com>
2024-02-09 11:17:17 +0200

    [tweaks] OneDrive Removal Update

--- tasks/system_setup/tweaks/remove_onedrive.ps1
+++ tasks/system_setup/tweaks/remove_onedrive.ps1
@@ -1,64 +1,83 @@
-Write-Host \"Kill OneDrive process\"
-taskkill.exe /F /IM \"OneDrive.exe\"
-taskkill.exe /F /IM \"explorer.exe\"
+# Kill OneDrive process
+Write-Host "Kill OneDrive process" -ForegroundColor Cyan
+taskkill.exe /F /IM "OneDrive.exe"
+taskkill.exe /F /IM "explorer.exe"
 
-Write-Host \"Copy all OneDrive to Root UserProfile\"
-Start-Process -FilePath robocopy -ArgumentList \"$env:USERPROFILE\\OneDrive $env:USERPROFILE /e /xj\" -NoNewWindow -Wait
+# Copy all OneDrive to Root UserProfile
+Write-Host "Copy all OneDrive to Root UserProfile" -ForegroundColor Cyan
+Start-Process -FilePath robocopy -ArgumentList "$env:USERPROFILE\OneDrive $env:USERPROFILE /e /xj" -NoNewWindow -Wait
 
-Write-Host \"Remove OneDrive\"
-Start-Process -FilePath winget -ArgumentList \"uninstall -e --purge --force --silent Microsoft.OneDrive \" -NoNewWindow -Wait
+# Remove OneDrive
+Write-Host "Remove OneDrive" -ForegroundColor Cyan
+Start-Process -FilePath winget -ArgumentList "uninstall -e --purge --force --silent Microsoft.OneDrive " -NoNewWindow -Wait
 
-Write-Host \"Removing OneDrive leftovers\"
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:localappdata\\Microsoft\\OneDrive\"
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:programdata\\Microsoft OneDrive\"
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:systemdrive\\OneDriveTemp\"
-# check if directory is empty before removing:
-if ((Get-ChildItem \"$env:userprofile\\OneDrive\" -Recurse | Measure-Object).Count -eq 0) {
-  Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:userprofile\\OneDrive\"
+# Removing OneDrive leftovers
+Write-Host "Removing OneDrive leftovers" -ForegroundColor Cyan
+$OneDriveFolders = @("$env:localappdata\Microsoft\OneDrive", "$env:programdata\Microsoft OneDrive", "$env:systemdrive\OneDriveTemp")
+$OneDriveFolders | ForEach-Object {
+    Remove-Item -Path $_ -Recurse -Force -ErrorAction SilentlyContinue
 }
 
-Write-Host \"Remove Onedrive from explorer sidebar\"
-Set-ItemProperty -Path \"HKCR:\\CLSID\\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\" -Name \"System.IsPinnedToNameSpaceTree\" -Value 0
-Set-ItemProperty -Path \"HKCR:\\Wow6432Node\\CLSID\\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\" -Name \"System.IsPinnedToNameSpaceTree\" -Value 0
+# Check if OneDrive directory is empty before removing
+if (!(Get-ChildItem "$env:userprofile\OneDrive" -Recurse | Measure-Object).Count) {
+    Remove-Item -Path "$env:userprofile\OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+}
 
-Write-Host \"Removing run hook for new users\"
-reg load \"hku\\Default\" \"C:\\Users\\Default\\NTUSER.DAT\"
-reg delete \"HKEY_USERS\\Default\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\" /v \"OneDriveSetup\" /f
-reg unload \"hku\\Default\"
+# Remove OneDrive from explorer sidebar
+Write-Host "Remove Onedrive from explorer sidebar" -ForegroundColor Cyan
+Set-ItemProperty -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+Set-ItemProperty -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
 
-Write-Host \"Removing startmenu entry\"
-Remove-Item -Force -ErrorAction SilentlyContinue \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\OneDrive.lnk\"
+# Removing run hook for new users
+Write-Host "Removing run hook for new users" -ForegroundColor Cyan
+reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
+reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
+reg unload "hku\Default"
 
-Write-Host \"Removing scheduled task\"
-Get-ScheduledTask -TaskPath '\\' -TaskName 'OneDrive*' -EA SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
+# Removing startmenu entry
+Write-Host "Removing startmenu entry" -ForegroundColor Cyan
+Remove-Item -Path "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force -ErrorAction SilentlyContinue
 
-# Add Shell folders restoring default locations
-Write-Host \"Shell Fixing\"
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"AppData\" -Value \"$env:userprofile\\AppData\\Roaming\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Cache\" -Value \"$env:userprofile\\AppData\\Local\\Microsoft\\Windows\\INetCache\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Cookies\" -Value \"$env:userprofile\\AppData\\Local\\Microsoft\\Windows\\INetCookies\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Favorites\" -Value \"$env:userprofile\\Favorites\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"History\" -Value \"$env:userprofile\\AppData\\Local\\Microsoft\\Windows\\History\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Local AppData\" -Value \"$env:userprofile\\AppData\\Local\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"My Music\" -Value \"$env:userprofile\\Music\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"My Video\" -Value \"$env:userprofile\\Videos\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"NetHood\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Network Shortcuts\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"PrintHood\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Printer Shortcuts\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Programs\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Recent\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Recent\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"SendTo\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\SendTo\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Start Menu\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Startup\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Templates\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Templates\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"{374DE290-123F-4565-9164-39C4925E467B}\" -Value \"$env:userprofile\\Downloads\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Desktop\" -Value \"$env:userprofile\\Desktop\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"My Pictures\" -Value \"$env:userprofile\\Pictures\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Personal\" -Value \"$env:userprofile\\Documents\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"{F42EE2D3-909F-4907-8871-4C22FC0BF756}\" -Value \"$env:userprofile\\Documents\" -Type ExpandString
-Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"{0DDD015D-B06C-45D5-8C4C-F59713854639}\" -Value \"$env:userprofile\\Pictures\" -Type ExpandString
-Write-Host \"Restarting explorer\"
-Start-Process \"explorer.exe\"
+# Removing scheduled task
+Write-Host "Removing scheduled task" -ForegroundColor Cyan
+Get-ScheduledTask -TaskPath '\' -TaskName 'OneDrive*' -ErrorAction SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
 
-Write-Host \"Waiting for explorer to complete loading\"
-Write-Host \"Please Note - OneDrive folder may still have items in it. You must manually delete it, but all the files should already be copied to the base user folder.\"
-Start-Sleep 5
+# Shell folders restoring default locations
+Write-Host "Shell Fixing" -ForegroundColor Cyan
+$shellFolders = @{
+    "AppData" = "$env:userprofile\AppData\Roaming";
+    "Cache" = "$env:userprofile\AppData\Local\Microsoft\Windows\INetCache";
+    "Cookies" = "$env:userprofile\AppData\Local\Microsoft\Windows\INetCookies";
+    "Favorites" = "$env:userprofile\Favorites";
+    "History" = "$env:userprofile\AppData\Local\Microsoft\Windows\History";
+    "Local AppData" = "$env:userprofile\AppData\Local";
+    "My Music" = "$env:userprofile\Music";
+    "My Video" = "$env:userprofile\Videos";
+    "NetHood" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Network Shortcuts";
+    "PrintHood" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Printer Shortcuts";
+    "Programs" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs";
+    "Recent" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Recent";
+    "SendTo" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\SendTo";
+    "Start Menu" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu";
+    "Startup" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup";
+    "Templates" = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Templates";
+    "{374DE290-123F-4565-9164-39C4925E467B}" = "$env:userprofile\Downloads";
+    "Desktop" = "$env:userprofile\Desktop";
+    "My Pictures" = "$env:userprofile\Pictures";
+    "Personal" = "$env:userprofile\Documents";
+    "{F42EE2D3-909F-4907-8871-4C22FC0BF756}" = "$env:userprofile\Documents";
+    "{0DDD015D-B06C-45D5-8C4C-F59713854639}" = "$env:userprofile\Pictures";
+}
+
+$shellFolders.GetEnumerator() | ForEach-Object {
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name $_.Key -Value $_.Value -Type ExpandString
+}
+
+# Restarting explorer
+Write-Host "Restarting explorer" -ForegroundColor Cyan
+Start-Process "explorer.exe"
+
+# Waiting for explorer to complete loading
+Write-Host "Waiting for explorer to complete loading" -ForegroundColor Cyan
+Write-Host "Please Note - OneDrive folder may still have items in it. You must manually delete it, but all the files should already be copied to the base user folder." -ForegroundColor Cyan
+Start-Sleep -Seconds 5
