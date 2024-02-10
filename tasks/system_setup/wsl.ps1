ce256295340549a79a9ee422e2e9e4e1f5038bac
jokerwrld999 <ifalaleev49@gmail.com>
2024-02-10 20:33:03 +0200

    [wsl] Ubuntu Job Updated

--- tasks/system_setup/wsl.ps1
+++ tasks/system_setup/wsl.ps1
@@ -162,6 +162,10 @@ function InstallUbuntuDistro {
     wsl -d Ubuntu -u root /bin/bash -c "apt -v >/dev/null 2>&1" | Out-Null
     if ($LASTEXITCODE -eq 0) {
       Write-Host "####### Ubuntu installed successfully. #######" -ForegroundColor Green
+      if (Get-Job -Name $jobName -ErrorAction SilentlyContinue) {
+        Stop-Job -Name $jobName | Out-Null
+        Remove-Job -Name $jobName | Out-Null
+      }
 
       wsl -d Ubuntu -u root /bin/bash -c "ansible --version >/dev/null 2>&1" | Out-Null
       if ($LASTEXITCODE -ne 0) {
@@ -178,17 +182,7 @@ function InstallUbuntuDistro {
         Write-Host "####### Initializing Ubuntu... #######" -ForegroundColor Blue
         Start-Job -Name $jobName -ScriptBlock { wsl --install -d Ubuntu } | Out-Null
       }
-
-      while ((Get-Job $jobName).State -ne 'Completed') {
-        Write-Host "Ubuntu initialization in progress..." -ForegroundColor Blue
-        Start-Sleep -Seconds 5  # Check every 5 seconds
-      }
-
-      if (Get-Job -Name $jobName -ErrorAction SilentlyContinue) {
-        Stop-Job -Name $jobName | Out-Null
-        Remove-Job -Name $jobName | Out-Null
-      }
-
+      Start-Sleep -s 20
     }
   }
 }
