d2701ebef110dfb89ecaa0effebe54a4854c10a1
jokerwrld999 <ifalaleev49@gmail.com>
2024-02-25 22:07:02 +0200

    [snapins] Ninite and Nvidia Updated

---
+++ files/terminal/snapins/ninite.ps1
@@ -0,0 +1,25 @@
+$jobName = "InstallNinite"
+$niniteTempPath= "C:\NiniteTemp"
+$niniteAppsSource = "$niniteTempPath\niniteN.exe"
+$niniteAppsRemote = "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/terminal/snapins/ninite/niniteN.exe"
+
+if (!(Test-Path -Path $niniteTempPath)) {
+  Write-Host "####### Downloading Ninite Apps... #######" -ForegroundColor Blue
+  New-Item -Path $niniteTempPath -ItemType Directory | Out-Null
+  Invoke-WebRequest -Uri $niniteAppsRemote -OutFile $niniteAppsSource
+}
+
+Write-Host "####### Installing Ninite Apps... #######" -ForegroundColor Blue
+Start-Job -Name $jobName -ScriptBlock {
+  Start-Process -WindowStyle hidden -FilePath "C:\NiniteTemp\niniteN.exe"
+} | Out-Null
+
+while ($true) {
+  if (((Get-Job -Name $jobName).State -eq "Completed")) {
+    Get-Process Ninite | Stop-Process
+    Remove-Job -Name $jobName
+    break
+  }
+  Start-Sleep 3
+}
+
--- files/terminal/snapins/nvidia.ps1
+++ files/terminal/snapins/nvidia.ps1
@@ -2,12 +2,15 @@ $nvTempDir = "C:\NvidiaTemp"
 $nvSrc = "$nvTempDir\driver.exe"
 $nvLatestVersion = "551.61"
 $nvRemote = "https://uk.download.nvidia.com/Windows/$nvLatestVersion/$nvLatestVersion-desktop-win10-win11-64bit-international-dch-whql.exe"
-$nvGetVersion = (Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.devicename -like "*nvidia*" -and $_.devicename -notlike "*audio*"}).DriverVersion | Select-Object -Last 1 | Out-String
-$nvCurrentVersion = ($nvGetVersion | Select-String -Pattern '.{7}$').Matches.Value.Replace(".","").Insert(3,'.').Trim()
 $nvDestUnzipPath = "$nvTempDir\$nvLatestVersion-Driver"
 $nvSrcUnzipPath = "$nvDestUnzipPath\setup.exe"
+$7zipRemote = "https://www.7-zip.org/a/7z2301-x64.exe"
+$7zipSrc = "$nvTempDir\7zip.exe"
 
 if ([bool]((Get-CimInstance win32_VideoController).Name | Select-String Nvidia)) {
+  $nvGetVersion = (Get-WmiObject Win32_PnPSignedDriver | Where-Object {$_.devicename -like "*nvidia*" -and $_.devicename -notlike "*audio*"}).DriverVersion | Select-Object -Last 1 | Out-String
+  $nvCurrentVersion = ($nvGetVersion | Select-String -Pattern '.{7}$').Matches.Value.Replace(".","").Insert(3,'.').Trim()
+
   if ($nvCurrentVersion -lt $nvLatestVersion){
     if (!(Test-Path -Path $nvSrc)) {
       Write-Host "####### Downloading Nvidia Driver... #######" -ForegroundColor Blue
@@ -16,6 +19,12 @@ if ([bool]((Get-CimInstance win32_VideoController).Name | Select-String Nvidia))
     }
 
     if (!(Test-Path -Path $nvSrcUnzipPath)) {
+      if (![bool](Get-Command 7z -ErrorAction SilentlyContinue)) {
+          Write-Host "####### Installing 7zip... #######" -ForegroundColor Blue
+          (New-Object System.Net.WebClient).DownloadFile($7zipRemote,$7zipSrc)
+          Start-Process -FilePath $7zipSrc -Args "/S" -Wait
+      }
+
       Write-Host "####### Extracting Nvidia Driver... #######" -ForegroundColor Blue
       Start-Process 7z.exe -ArgumentList "x $nvSrc `"-o$($nvDestUnzipPath)`" -y -bso0 -bd" -NoNewWindow -Wait
     }
@@ -33,3 +42,4 @@ if ([bool]((Get-CimInstance win32_VideoController).Name | Select-String Nvidia))
 } else {
     Write-Host "####### There was no any Nvidia Card found. #######" -ForegroundColor DarkMagenta
 }
+
