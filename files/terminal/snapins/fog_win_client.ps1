03bae2efbd9119e558bb3552d355b0b3b413fb36
jokerwrld999 <ifalaleev49@gmail.com>
2024-02-26 13:37:47 +0200

    [snapins] Fog Client Service Update

--- files/terminal/snapins/fog_win_client.ps1
+++ files/terminal/snapins/fog_win_client.ps1
@@ -1,12 +1,15 @@
 $fogTempDir = "C:\fogtemp"
 $fogMsiPath = "$fogTempDir\fog.msi"
+$fogInstalledPath = "C:\Program Files (x86)\FOG\FOGService.exe"
 $fogserver = "10.2.252.200"
 
 if (!(Test-Path $fogMsiPath)) {
   New-Item -Type Directory -Path $fogTempDir
-  (New-Object System.Net.WebClient).DownloadFile("http://$fogserver/fog/client/download.php?newclient",$fogMsiPath)
-  Start-Process msiexec.exe "/i $fogMsiPath /quiet /norestart /qn USETRAY=`"0`" WEBADDRESS=`"10.2.252.200`"" -Wait
-  Get-Service "*Fog*" | Set-Service -StartupType Automatic
+  if (!(Test-Path $fogInstalledPath)) {
+    (New-Object System.Net.WebClient).DownloadFile("http://$fogserver/fog/client/download.php?newclient",$fogMsiPath)
+    Start-Process msiexec.exe "/i $fogMsiPath /quiet /norestart /qn USETRAY=`"0`" WEBADDRESS=`"$fogserver`"" -Wait
+  }
+  Get-Service "*Fog*" |  Set-Service -Status Running -StartupType Automatic
   Get-Service "*Fog*" | Start-Service
   Remove-Item -Path $fogTempDir -Recurse -ErrorAction SilentlyContinue -Force
 }
