cab77fdd9aaaf0182340e0969697ec2b832d34e7
jokerwrld999 <ifalaleev49@gmail.com>
2024-02-26 13:05:16 +0200

    [snapins] Fog Client Install

--- files/terminal/snapins/fog_win_client.ps1
+++ files/terminal/snapins/fog_win_client.ps1
@@ -5,6 +5,8 @@ $fogserver = "10.2.252.200"
 if (!(Test-Path $fogMsiPath)) {
   New-Item -Type Directory -Path $fogTempDir
   (New-Object System.Net.WebClient).DownloadFile("http://$fogserver/fog/client/download.php?newclient",$fogMsiPath)
-  Start-Process -FilePath msiexec -ArgumentList @('/i',$fogMsiPath,'/quiet','/qn','/norestart') -NoNewWindow -Wait
+  Start-Process msiexec.exe "/i $fogMsiPath /quiet /norestart /qn USETRAY=`"0`" WEBADDRESS=`"10.2.252.200`"" -Wait
+  Get-Service "*Fog*" | Set-Service -StartupType Automatic
+  Get-Service "*Fog*" | Start-Service
   Remove-Item -Path $fogTempDir -Recurse -ErrorAction SilentlyContinue -Force
 }
