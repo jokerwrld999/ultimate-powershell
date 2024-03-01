bef5de6773c0e6ca486f915e4b1ec9390a780c50
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-01 19:09:01 +0200

    [snapins] WS Drivers Script update

--- files/snapins/main_z640_drivers.ps1
+++ files/snapins/main_z640_drivers.ps1
@@ -1,3 +1,2 @@
-Invoke-RestMethod "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/snapins/scoop_packages.ps1" | Invoke-Expression
 Invoke-RestMethod "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/snapins/nvidia.ps1" | Invoke-Expression
 Invoke-RestMethod "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/snapins/hp_z640_drivers.ps1" | Invoke-Expression
