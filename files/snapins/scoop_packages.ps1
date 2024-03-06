bbb08c9ab9afdb54628a5600734ab1392445c5ea
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-06 10:49:50 +0200

    [snapins] Scoop | Fixed

--- files/snapins/scoop_packages.ps1
+++ files/snapins/scoop_packages.ps1
@@ -1,40 +1,33 @@
 #Requires -RunAsAdministrator
 
-Write-Host "Current User: $(whoami)"
-Write-Host "Current Work Dir: $(pwd)"
-
 $ExecutionPolicy = Get-ExecutionPolicy -Scope CurrentUser
 if ($ExecutionPolicy -ne "RemoteSigned") {
   Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 }
 
-
-$env:SCOOP_GLOBAL="$env:ProgramData\GlobalScoopApps"
-[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
-$env:SCOOP="$env:ProgramData\Scoop"
-[Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'MACHINE')
-$Reg='Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment'
-$OldPath=(Get-ItemProperty -Path $Reg -Name PATH).Path
-$NewPath="$OldPath;$env:SCOOP\shims"
-Set-ItemProperty -Path $Reg -Name PATH -Value $NewPath
-$CurrentValue=[Environment]::GetEnvironmentVariable('PSModulePath','Machine')
-[Environment]::SetEnvironmentVariable('PSModulePath', $CurrentValue + ";$env:SCOOP\modules", 'Machine')
-
-
-
 if (![bool](Get-Command -Name 'scoop' -ErrorAction SilentlyContinue)) {
+  $env:SCOOP_GLOBAL="$env:ProgramData\GlobalScoopApps"
+  [Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
+  $env:SCOOP="$env:ProgramData\Scoop"
+  [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'MACHINE')
+  $Reg='Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment'
+  $OldPath=(Get-ItemProperty -Path $Reg -Name PATH).Path
+  $NewPath="$OldPath;$env:SCOOP\shims"
+  Set-ItemProperty -Path $Reg -Name PATH -Value $NewPath
+  $CurrentValue=[Environment]::GetEnvironmentVariable('PSModulePath','Machine')
+  [Environment]::SetEnvironmentVariable('PSModulePath', $CurrentValue + ";$env:SCOOP\modules", 'Machine')
   Write-Host "Installing Scoop Module..." -ForegroundColor Blue
   Invoke-Expression "& {$(Invoke-RestMethod 'https://get.scoop.sh')} -RunAsAdmin"
   scoop install gsudo git scoop-search -g *> $null
 }
 
-# $scoopAppsBucket = 'Scoop-Apps'
-# if (!(scoop bucket list | Select-String $scoopAppsBucket)) {
-#   Write-Host "Setting Scoop Apps Custom Bucket" -ForegroundColor Blue
-#   scoop config SCOOP_REPO 'https://github.com/Ash258/Scoop-Core' *> $null
-#   scoop bucket add 'Base' *> $null
-#   scoop update *> $null
-# }
+$scoopAppsBucket = 'Scoop-Apps'
+if (!(scoop bucket list | Select-String $scoopAppsBucket)) {
+  Write-Host "Setting Scoop Apps Custom Bucket" -ForegroundColor Blue
+  scoop config SCOOP_REPO 'https://github.com/Ash258/Scoop-Core' *> $null
+  scoop bucket add 'Base' *> $null
+  scoop update *> $null
+}
 
 $buckets = @('main','extras')
 foreach ($bucket in $buckets) {
