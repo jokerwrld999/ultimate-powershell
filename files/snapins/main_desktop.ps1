801d576f6428f0b982728953be3c9e5c9b910905
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-06 14:02:05 +0200

    [snapins] Desktop Pack

---
+++ files/snapins/main_desktop.ps1
@@ -0,0 +1,3 @@
+Invoke-RestMethod "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/snapins/scoop_packages.ps1" | Invoke-Expression
+Invoke-RestMethod "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/snapins/nvidia.ps1" | Invoke-Expression
+Invoke-RestMethod "https://github.com/jokerwrld999/ultimate-powershell/raw/main/files/snapins/desktop_chipset_drivers.ps1" | Invoke-Expression
