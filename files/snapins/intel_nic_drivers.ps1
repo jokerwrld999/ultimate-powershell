a7249723b3b1749ba51552507d6b5364341e9fe0
jokerwrld999 <ifalaleev49@gmail.com>
2024-03-06 11:37:05 +0200

    [snapins] Intel NIC drivers | Update

--- files/snapins/intel_nic_drivers.ps1
+++ files/snapins/intel_nic_drivers.ps1
@@ -1,7 +1,7 @@
 $driverSrcPath = "C:\drivers"
 $drivers = @(
   @{ Name = "Intel Ethernet Adapter";
-    driverID = "$(pnputil /enum-devices /problem | Select-String 'PCI\VEN_8086&DEV_15F3')";
+    driverID = "$(pnputil /enum-devices /problem | Select-String 'VEN_8086&DEV_15F3')";
     installSwitches = "/add-driver `"$driverSrcPath\*.inf`" /subdirs /install"
   }
 )
@@ -9,7 +9,7 @@ $drivers = @(
 foreach ($driver in $drivers) {
   if ([bool]$driver.driverID) {
     Write-Host "####### Installing $($driver.Name) Driver... #######" -ForegroundColor Blue
-    Start-Process PNPUTIL.exe -ArgumentList $driver.installSwitches -Wait
+    Start-Process PNPUTIL.exe -ArgumentList $driver.installSwitches -Wait -NoNewWindow | Out-Null
   } else {
       Write-Host "####### $($driver.Name) Driver has been already installed. #######" -ForegroundColor Green
   }
@@ -18,3 +18,5 @@ foreach ($driver in $drivers) {
 if (Test-Path -Path $driverSrcPath) {
   Remove-Item -Path $driverSrcPath -Recurse -Force
 }
+
+Restart-Computer -Force
